e31d5c49dcd64b6a4577ce690d9ef337de9c4b2f
Dustin Barnett <dustin.barnett@DTG.local>
2019-09-24 09:26:54 -0400

    First commit

---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    name: 'OnePlus 7 Pro',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    image: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQR0-FDikhHNx5ZB3SvziqePKp9S3cDpa0HMPnM8uOQjy7EI13xMtP0UbossaHdliaNpCLV_IcsL0j2N-YKR-Z4YhWgZrNUR3dZRhkPLHVWuWPnXaiInwFyfw&usqp=CAc'
+  },
+  {
+    name: 'iPhone 11',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    image: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTpzghKHGzEXy4rx386nsh8OHxYR9ZKrVS1DrzkQpRICbyGYh-MUakFWF1yMW3PZJp82LhqzKtPdr55dCPnMpF76riRZc7SwyUTOlhlDZASueE7AvoVRWTqrg&usqp=CAc'
+  },
+  {
+    name: 'Blackberry',
+    price: 299,
+    description: '',
+    image: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSylFBY6iF2MiWS2rzjl0wVJ3OE_iV1DdlxkAJlgbdEPNBBtJXeZQO185MA8mYsRB-3wlUFi-YR9SSgAbDYueFmkLV83VPmd_4OjMKbyHmqgq65Pq_kyuI&usqp=CAc'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
